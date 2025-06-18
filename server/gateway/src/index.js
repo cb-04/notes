f24d5c93dba9d380c3b42a7bd4f371f235adf255
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-06-18 20:53:26 +0530

    Hello World Server

---
+++ server/gateway/src/index.js
@@ -0,0 +1,14 @@
+/** Main application router for gateway */
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    console.log('get /');
+    res.send('Hello world!');
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => {
+    console.log(`gateway is listening on port ${port}`);
+});
