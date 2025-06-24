5127cef571941e89964921b238381816bcb7d8b6
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-06-24 19:37:50 +0530

    Connect to database from server

--- server/gateway/src/Utils.ts
+++ server/gateway/src/Utils.ts
@@ -4,30 +4,72 @@
  */
 
 /**
- * Gets current date and time in the ISO format
+ * Gets current date and time in ISO format
  * 
  * @returns date time in ISO string format
  */
-
-export function getDateTime():string {
-    const date = new Date(Date.now()); //Using Date.now() to make the code testable
-    return (date.toISOString());
-}
+export function getDateTime(): string {
+    const date = new Date(Date.now());  // Use Date.now() to make code testable
+    return date.toISOString();
+  }
 
 /**
  * Converts an array into an object with each property of the object
  * set to an array element.
+ * 
  * @param array : array to convert into an object
- * @param key : defines which item in the array is to become the key
+ * @param key: defines which item in the array is to become the key
  * @returns object derived from the array
  */
+export function array2Obj(
+  array: Array<Record<string,string>>, key: string) 
+  : Record<string,Record<string,string>> {
+  const initialValue = {};
+  return array.reduce(
+    (obj: Record<string,Record<string,string>>, item: Record<string,string>) => {
+    return {
+      ...obj,
+      [item[key]]: item,
+    };
+  }, initialValue);
+}
 
-export function array2Obj(array: Array<Record<string,string>>, key:string) : Record<string,Record<string,string>> {
-    const initialValue = {};
-    return array.reduce((obj:Record<string,Record<string,string>>, item:Record<string,string>) => {
-        return {
-            ...obj,
-            [item[key]]: item,
-        };
-    }, initialValue);
+/**
+ * Class DBConnect
+ * Connects to the Database
+ * 
+ * Supply username, password & db url via environment variables
+ */
+import axios from 'axios';
+export class DBConnect {
+  username: string;
+  password: string;
+  url: string;
+
+  constructor() {
+    this.username = process.env.dbUsername;
+    this.password = process.env.dbPassword;
+    this.url = process.env.dbUrl;
+  }
+
+  /** Returns httpConfig needed by axios */
+  getHttpConfig() : unknown {
+    const credentials = this.username + ':' + this.password;
+    const auth = "Basic " + Buffer.from(credentials).toString('base64');
+    const httpConfig = {
+      headers: {
+        "Content-Type":"application/json",
+        "authorization": auth,
+        "cache-control":"no-cache"
+      }
+    };
+    return( httpConfig );
+  }
+
+  /** send function sends http post to database */
+  async send(query: unknown) : Promise<unknown> {
+    const httpConfig = this.getHttpConfig();
+    const response = await axios.post(this.url, query, httpConfig);
+    return( response.data );
+  }
 }
